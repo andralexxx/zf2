0363f3deb3a88b632785412f34dc0d874f2e0069
jc_agrigoriev <agrigoriev@justcoded.com>
2012-09-24 16:48:28 +0300

    Database and models NOT working yet

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -1,5 +1,6 @@
 <?php
 // module/Album/Module.php
+
 namespace Album;
 
 class Module
@@ -22,4 +23,17 @@ class Module
 	{
 		return include __DIR__ . '/config/module.config.php';
 	}
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Album\Model\AlbumTable' =>  function($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $table     = new AlbumTable($dbAdapter);
+                    return $table;
+                },
+            ),
+        );
+    }
 }
